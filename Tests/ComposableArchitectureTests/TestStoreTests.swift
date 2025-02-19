b68ff2a237b92389504db9682f66a24b9eea9074
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-18 19:23:42 -0600

    Update TestStoreTests.swift

--- Tests/ComposableArchitectureTests/TestStoreTests.swift
+++ Tests/ComposableArchitectureTests/TestStoreTests.swift
@@ -1,209 +1,75 @@
-#if swift(>=5.9)
-  import Combine
-  import ComposableArchitecture
-  import XCTest
+Void
+End
+Delete
 
-  final class TestStoreTests: BaseTCATestCase {
-    @MainActor
-    func testEffectConcatenation() async {
-      struct State: Equatable {}
 
-      enum Action: Equatable {
-        case a, b1, b2, b3, c1, c2, c3, d
-      }
 
-      let mainQueue = DispatchQueue.test
-      let store = TestStore(initialState: State()) {
-        Reduce<State, Action> { _, action in
-          switch action {
-          case .a:
-            return .merge(
-              .run { send in
-                try await mainQueue.sleep(for: .seconds(1))
-                await send(.b1)
-                await send(.c1)
-              },
-              .run { _ in try await Task.never() }
-                .cancellable(id: 1)
-            )
-          case .b1:
-            return .concatenate(.send(.b2), .send(.b3))
-          case .c1:
-            return .concatenate(.send(.c2), .send(.c3))
-          case .b2, .b3, .c2, .c3:
-            return .none
-          case .d:
-            return .cancel(id: 1)
-          }
-        }
-      }
 
-      await store.send(.a)
 
-      await mainQueue.advance(by: 1)
 
-      await store.receive(.b1)
-      await store.receive(.b2)
-      await store.receive(.b3)
 
-      await store.receive(.c1)
-      await store.receive(.c2)
-      await store.receive(.c3)
 
-      await store.send(.d)
-    }
 
-    @MainActor
-    func testAsync() async {
-      enum Action: Equatable {
-        case tap
-        case response(Int)
-      }
-      let store = TestStore(initialState: 0) {
-        Reduce<Int, Action> { state, action in
-          switch action {
-          case .tap:
-            return .run { send in await send(.response(42)) }
-          case let .response(number):
-            state = number
-            return .none
-          }
-        }
-      }
 
-      await store.send(.tap)
-      await store.receive(.response(42)) {
-        $0 = 42
-      }
-    }
 
-    @MainActor
-    func testExpectedStateEquality() async {
-      struct State: Equatable {
-        var count: Int = 0
-        var isChanging: Bool = false
-      }
 
-      enum Action: Equatable {
-        case increment
-        case changed(from: Int, to: Int)
-      }
 
-      let store = TestStore(initialState: State()) {
-        Reduce<State, Action> { state, action in
-          switch action {
-          case .increment:
-            state.isChanging = true
-            return .send(.changed(from: state.count, to: state.count + 1))
-          case let .changed(from, to):
-            state.isChanging = false
-            if state.count == from {
-              state.count = to
-            }
-            return .none
-          }
-        }
-      }
 
-      await store.send(.increment) {
-        $0.isChanging = true
-      }
-      await store.receive(.changed(from: 0, to: 1)) {
-        $0.isChanging = false
-        $0.count = 1
-      }
 
-      XCTExpectFailure()
-      await store.send(.increment) {
-        $0.isChanging = false
-      }
 
-      XCTExpectFailure()
-      await store.receive(.changed(from: 1, to: 2)) {
-        $0.isChanging = true
-        $0.count = 1100
-      }
-    }
 
-    @MainActor
-    func testExpectedStateEqualityMustModify() async {
-      struct State: Equatable {
-        var count: Int = 0
-      }
 
-      enum Action: Equatable {
-        case noop, finished
-      }
 
-      let store = TestStore(initialState: State()) {
-        Reduce<State, Action> { state, action in
-          switch action {
-          case .noop:
-            return .send(.finished)
-          case .finished:
-            return .none
-          }
-        }
-      }
 
-      await store.send(.noop)
-      await store.receive(.finished)
 
-      XCTExpectFailure()
-      await store.send(.noop) {
-        $0.count = 0
-      }
 
-      XCTExpectFailure()
-      await store.receive(.finished) {
-        $0.count = 0
-      }
-    }
 
-    @MainActor
-    func testReceiveActionMatchingPredicate() async {
-      enum Action: Equatable {
-        case noop, finished
-      }
 
-      let store = TestStore(initialState: 0) {
-        Reduce<Int, Action> { state, action in
-          switch action {
-          case .noop:
-            return .send(.finished)
-          case .finished:
-            return .none
-          }
-        }
-      }
 
-      let predicateShouldBeCalledExpectation = expectation(
-        description: "predicate should be called")
-      await store.send(.noop)
-      await store.receive { action in
-        predicateShouldBeCalledExpectation.fulfill()
-        return action == .finished
-      }
-      _ = { wait(for: [predicateShouldBeCalledExpectation], timeout: 0) }()
 
-      await store.send(.noop)
-      XCTExpectFailure()
-      await store.receive(.noop)
 
-      await store.send(.noop)
-      XCTExpectFailure()
-      await store.receive { $0 == .noop }
-    }
 
-    @MainActor
-    func testStateAccess() async {
-      enum Action { case a, b, c, d }
-      let store = TestStore(initialState: 0) {
-        Reduce<Int, Action> { count, action in
-          switch action {
-          case .a:
-            count += 1
-            return .merge(.send(.b), .send(.c), .send(.d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           case .b, .c, .d:
             count += 1
             return .none
