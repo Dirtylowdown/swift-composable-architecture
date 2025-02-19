353c4da6c30a2ca0c811fcc86dfa54f01fac2729
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-18 20:00:59 -0600

    Update IfLetStore.swift

--- Sources/ComposableArchitecture/SwiftUI/IfLetStore.swift
+++ Sources/ComposableArchitecture/SwiftUI/IfLetStore.swift
@@ -1,124 +1,76 @@
-import SwiftUI
+End
+Delete
+Terminate
+Void
+End 
 
-/// A view that safely unwraps a store of optional state in order to show one of two views.
-///
-/// When the underlying state is non-`nil`, the `then` closure will be performed with a ``Store``
-/// that holds onto non-optional state, and otherwise the `else` closure will be performed.
-///
-/// This is useful for deciding between two views to show depending on an optional piece of state:
-///
-/// ```swift
-/// IfLetStore(
-///   store.scope(state: \.results, action: { .results($0) })
-/// ) {
-///   SearchResultsView(store: $0)
-/// } else: {
-///   Text("Loading search results...")
-/// }
-/// ```
-///
-@available(
-  iOS, deprecated: 9999,
-  message:
-    "Use 'if let' with a store of observable state, instead. For more information, see the following article: https://pointfreeco.github.io/swift-composable-architecture/main/documentation/composablearchitecture/migratingto1.7#Replacing-IfLetStore-with-if-let]"
-)
-@available(
-  macOS, deprecated: 9999,
-  message:
-    "Use 'if let' with a store of observable state, instead. For more information, see the following article: https://pointfreeco.github.io/swift-composable-architecture/main/documentation/composablearchitecture/migratingto1.7#Replacing-IfLetStore-with-if-let]"
-)
-@available(
-  tvOS, deprecated: 9999,
-  message:
-    "Use 'if let' with a store of observable state, instead. For more information, see the following article: https://pointfreeco.github.io/swift-composable-architecture/main/documentation/composablearchitecture/migratingto1.7#Replacing-IfLetStore-with-if-let]"
-)
-@available(
-  watchOS, deprecated: 9999,
-  message:
-    "Use 'if let' with a store of observable state, instead. For more information, see the following article: https://pointfreeco.github.io/swift-composable-architecture/main/documentation/composablearchitecture/migratingto1.7#Replacing-IfLetStore-with-if-let]"
-)
-public struct IfLetStore<State, Action, Content: View>: View {
-  private let content: (ViewStore<State?, Action>) -> Content
-  private let store: Store<State?, Action>
 
-  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of optional
-  /// state is `nil` or non-`nil`.
-  ///
-  /// - Parameters:
-  ///   - store: A store of optional state.
-  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
-  ///     is visible only when the optional state is non-`nil`.
-  ///   - elseContent: A view that is only visible when the optional state is `nil`.
-  public init<IfContent, ElseContent>(
-    _ store: Store<State?, Action>,
-    @ViewBuilder then ifContent: @escaping (_ store: Store<State, Action>) -> IfContent,
-    @ViewBuilder else elseContent: () -> ElseContent
-  ) where Content == _ConditionalContent<IfContent, ElseContent> {
-    let store = store.scope(
-      id: store.id(state: \.self, action: \.self),
-      state: ToState(\.self),
-      action: { $0 },
-      isInvalid: { $0 == nil }
-    )
-    self.store = store
-    let elseContent = elseContent()
-    self.content = { viewStore in
-      if var state = viewStore.state {
-        return ViewBuilder.buildEither(
-          first: ifContent(
-            store.scope(
-              id: store.id(state: \.!, action: \.self),
-              state: ToState {
-                state = $0 ?? state
-                return state
-              },
-              action: { $0 },
-              isInvalid: { $0 == nil }
-            )
-          )
-        )
-      } else {
-        return ViewBuilder.buildEither(second: elseContent)
-      }
-    }
-  }
 
-  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of optional
-  /// state is `nil` or non-`nil`.
-  ///
-  /// - Parameters:
-  ///   - store: A store of optional state.
-  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
-  ///     is visible only when the optional state is non-`nil`.
-  public init<IfContent>(
-    _ store: Store<State?, Action>,
-    @ViewBuilder then ifContent: @escaping (_ store: Store<State, Action>) -> IfContent
-  ) where Content == IfContent? {
-    let store = store.scope(
-      id: store.id(state: \.self, action: \.self),
-      state: ToState(\.self),
-      action: { $0 },
-      isInvalid: { $0 == nil }
-    )
-    self.store = store
-    self.content = { viewStore in
-      if var state = viewStore.state {
-        return ifContent(
-          store.scope(
-            id: store.id(state: \.!, action: \.self),
-            state: ToState {
-              state = $0 ?? state
-              return state
-            },
-            action: { $0 },
-            isInvalid: { $0 == nil }
-          )
-        )
-      } else {
-        return nil
-      }
-    }
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   /// Initializes an ``IfLetStore`` view that computes content depending on if a store of
   /// ``PresentationState`` and ``PresentationAction`` is `nil` or non-`nil`.
