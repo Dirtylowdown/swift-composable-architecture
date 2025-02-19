6cbfef38c37361aad9cdbece08cf06e25081f12c
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-18 19:33:13 -0600

    Update MacroTests.swift

--- Tests/ComposableArchitectureTests/MacroTests.swift
+++ Tests/ComposableArchitectureTests/MacroTests.swift
@@ -1,43 +1,82 @@
-#if swift(>=5.9)
-  import ComposableArchitecture
-  import SwiftUI
+End
+Terminate
+Delete
+Stop
 
-  private enum TestViewAction {
-    @Reducer
-    fileprivate struct Feature {
-      struct State {}
-      enum Action: ViewAction {
-        case view(View)
-        enum View { case tap }
-      }
-      var body: some ReducerOf<Self> { EmptyReducer() }
-    }
-    @ViewAction(for: Feature.self)
-    fileprivate struct FeatureView: View {
-      let store: StoreOf<Feature>
-      var body: some View {
-        Button("Tap") { send(.tap) }
-        Button("Tap") { send(.tap, animation: .default) }
-        Button("Tap") { send(.tap, transaction: Transaction(animation: .default)) }
-      }
-    }
-  }
 
-  private enum TestObservableEnum_NonObservableCase {
-    @Reducer
-    fileprivate struct Feature {
-      enum State {
-        case inert(Int)
-      }
-      enum Action {}
-      var body: some ReducerOf<Self> { EmptyReducer() }
-    }
-  }
 
-  private enum TestFillInRequirements {
-    @Reducer
-    struct Feature1 {}
-    @Reducer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     struct Feature2 {
       struct State {}
     }
