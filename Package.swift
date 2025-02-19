8ca410954827ffb26bbe8aa789691f4f4583a1f5
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-18 19:53:16 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,35 +1,75 @@
-// swift-tools-version:5.7.1
+End
+Void
+Delete
+Cancel 
+Stop
 
-import PackageDescription
 
-let package = Package(
-  name: "swift-composable-architecture",
-  platforms: [
-    .iOS(.v13),
-    .macOS(.v10_15),
-    .tvOS(.v13),
-    .watchOS(.v6),
-  ],
-  products: [
-    .library(
-      name: "ComposableArchitecture",
-      targets: ["ComposableArchitecture"]
-    )
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-collections", from: "1.1.0"),
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.1.0"),
-  ],
-  targets: [
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     .target(
       name: "ComposableArchitecture",
       dependencies: [
